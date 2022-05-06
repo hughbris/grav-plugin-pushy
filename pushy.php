2ac6a374efb2e7691860f7263f9c9b10984ecbdc
Hugh Barnes <dev@hubns.com>
2022-05-06 20:41:33 +1200

    Add stub webhook server

--- pushy.php
+++ pushy.php
@@ -62,7 +62,7 @@ class PushyPlugin extends Plugin {
 
 		else {
 			$this->enable([
-				// Put your main events here
+				'onPageInitialized'      => ['serveHooks', 0],
 				]);
 		}
 	}
@@ -106,6 +106,120 @@ class PushyPlugin extends Plugin {
 			$twig = $this->grav['twig'];
 			$twig->twig_vars['git_index'] = $this->repo->statusSelect(); # TRUE, $env='index', $select='MTDRCA');
 		}
+	}
+
+	public function serveHooks() {
+
+		$webhooks = $this->config->get("plugins.{$this->name}.webhooks");
+		if (!($webhooks['enabled'] ?? FALSE)) {
+			return;
+		}
+		$page = $this->grav['page'] ?? NULL; // CHECKME: may not need this
+		// dump($this->grav['uri']->uri(), is_null($page->route())); return;
+
+		if (/* is_null($page->route()) && */ $this->grav['uri']->uri() == $webhooks['path']) { // TODO: just check for uri starting with path here
+			if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+				header('Content-Type: application/json');
+				if ($webhooks['secret'] ?? false) {
+					if (!$this->isWebhookAuthenticated($webhooks['secret'])) {
+						http_response_code(401);
+						// TODO: 'WWW-Authenticate' header here??
+						echo json_encode([
+							'status' => 'error',
+							'message' => 'Unauthorized request',
+							]);
+						exit;
+					}
+				}
+
+				// TODO: parse the request for branch/tag and do other condition filtering here - respond with 202 and a "void" status or something (possibly even https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/204 ??)
+
+				// TODO: possibly branch into other hooks here, not just /pull
+				try {
+					# $this->synchronize();
+					http_response_code(202);
+					echo json_encode([
+						'status' => 'success',
+						'message' => 'GitSync completed the synchronization',
+						]);
+				}
+				catch (\Exception $e) {
+					http_response_code(500);
+					echo json_encode([
+						'status' => 'error',
+						'message' => 'GitSync failed to synchronize',
+						]);
+				}
+			}
+			else {
+				http_response_code(405);
+			}
+			exit;
+		}
+	}
+
+	/**
+	 * Returns true if the request contains a valid signature or token
+	 * @param  string $secret local secret
+	 * @return bool           whether or not the request is authorized
+	 */
+	// copied from GitSync base class method isRequestAuthorized()
+	public function isWebhookAuthenticated($secret): bool {
+		if (isset($_SERVER['HTTP_X_HUB_SIGNATURE'])) {
+			$payload = file_get_contents('php://input') ?: '';
+
+			return $this->isGithubSignatureValid($secret, $_SERVER['HTTP_X_HUB_SIGNATURE'], $payload);
+		}
+
+		if (isset($_SERVER['HTTP_X_GITLAB_TOKEN'])) {
+			return $this->isGitlabTokenValid($secret, $_SERVER['HTTP_X_GITLAB_TOKEN']);
+		}
+		else {
+			$payload = file_get_contents('php://input');
+			return $this->isGiteaSecretValid($secret, $payload);
+		}
+
+		return FALSE;
+	}
+
+	/**
+	 * Hashes the webhook request body with the client secret and
+	 * checks if it matches the webhook signature header
+	 * @param  string $secret The webhook secret
+	 * @param  string $signatureHeader The signature of the webhook request
+	 * @param  string $payload The webhook request body
+	 * @return bool            whether the signature is valid or not
+	 */
+	private function isGithubSignatureValid($secret, $signatureHeader, $payload): bool {
+		[$algorithm, $signature] = explode('=', $signatureHeader);
+
+		return $signature === hash_hmac($algorithm, $payload, $secret); // TODO: try hash_equals($signature, hash_hmac($algorithm, $payload, $secret)), more secure
+	}
+
+	/**
+	 * Returns true if given Gitlab token matches secret
+	 * @param  string $secret local secret
+	 * @param  string $token token received from Gitlab webhook request
+	 * @return bool          whether or not secret and token match
+	 */
+	private function isGitlabTokenValid($secret, $token): bool {
+		return $secret === $token; // TODO: try hash_equals($secret === $token), more secure
+	}
+
+	/**
+	 * Returns true if secret contained in the payload matches the client
+	 * secret
+	 * @param  string $secret The webhook secret
+	 * @param  string $payload The webhook request body
+	 * @return bool            whether the client secret matches the payload secret or
+	 * not
+	 */
+	private function isGiteaSecretValid($secret, $payload): bool {
+		$payload = json_decode($payload, TRUE);
+		if (!empty($payload) && isset($payload['secret'])) {
+			return $secret === $payload['secret']; // TODO: try hash_equals($secret, $payload['secret']), more secure
+		}
+		return FALSE;
 	}
 
 }
